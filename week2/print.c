f4231962b62e8c9b9502662b3d7d9da7b6519a17
Jwhyee <kawnsdud@naver.com>
2022-06-16 14:20:30 +0900

    C 출력 문제 수정

--- week2/print.c
+++ week2/print.c
@@ -1,7 +1,26 @@
+// 문제 : `안녕하세요.`를 10줄 출력해주세요.
 #include <stdio.h>
-int main(void){
-    
-    printf("안녕하세요\n");
 
-    return 0;
+#pragma warning (disable: 4996)
+
+// 개념설명 시작
+// Ctrl + Enter 를 하면 main 함수가 실행된다.
+// '\n' => 뉴라인, 줄바꿈
+// printf("안녕"); => 안녕이 화면에 나온다.
+// 개념설명 끝
+
+// 아래 함수를 메인함수라고 부른다.
+int main(void) {
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  printf("안녕하세요.\n");
+  
+  return 0;
 }
