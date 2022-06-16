da80194b014262dba3c74a81e0cc073659dd3e3f
Jwhyee <kawnsdud@naver.com>
2022-06-16 14:09:33 +0900

    C 변수사용 문제

---
+++ week2/variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int x = 20;
+    x = x - 10;
+
+    int a = 20;
+    int b = 40;
+
+    a += 20;
+    b -= 20;
+
+    printf("a : %d\n", a);
+    printf("b : %d\n", b);
+
+    return 0;
+}
