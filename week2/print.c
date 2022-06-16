30de6ee8d0621f1e82a3c3802119a90a869d5fe0
Jwhyee <kawnsdud@naver.com>
2022-06-16 14:22:02 +0900

    개념 설명 삭제

--- week2/print.c
+++ week2/print.c
@@ -3,13 +3,6 @@
 
 #pragma warning (disable: 4996)
 
-// 개념설명 시작
-// Ctrl + Enter 를 하면 main 함수가 실행된다.
-// '\n' => 뉴라인, 줄바꿈
-// printf("안녕"); => 안녕이 화면에 나온다.
-// 개념설명 끝
-
-// 아래 함수를 메인함수라고 부른다.
 int main(void) {
   printf("안녕하세요.\n");
   printf("안녕하세요.\n");
