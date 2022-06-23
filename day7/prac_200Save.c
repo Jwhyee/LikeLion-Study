c0e76ebd76508d134ef51c852cbb6646dc3349b5
Jwhyee <kawnsdud@naver.com>
2022-06-23 10:23:45 +0900

    연습문제 업로드

---
+++ day7/prac_200Save.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+   
+
+int main(void) {
+
+    unsigned char i = 200;
+    // -128 ~ 127 -> 0~255로 음수를 양수로 몰빵해준다.
+    
+    printf("i = %u\n", i);
+  
+  return 0;
+}
---
+++ day7/prac_piSave.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+   
+
+int main(void) {
+  int i = 3;
+  
+  printf("1st 시도 => i : %d\n", i); 
+
+  float f = 3.14;
+  
+  printf("2nd 시도 => f : %.2f\n", f);
+
+  // 여기서 구현
+  
+  return 0;
+}
