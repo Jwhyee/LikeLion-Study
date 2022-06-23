b65bcfc7585484e9e49de670e3549a1fc2298c84
Jwhyee <kawnsdud@naver.com>
2022-06-23 16:38:46 +0900

    데이터 타입 알아보기

--- day7/prac_200Save.c
+++ day7/prac_200Save.c
@@ -5,8 +5,24 @@ int main(void) {
 
     unsigned char i = 200;
     // -128 ~ 127 -> 0~255로 음수를 양수로 몰빵해준다.
-    
+
     printf("i = %u\n", i);
+
+    int s1 = 2100000000;
+    // 21억
+    printf("s = %d\n", s1);
+
+    unsigned int s2 = 2200000000;
+    // 22억
+    printf("s = %u\n", s2);
+
+    unsigned int s3 = 4200000000;
+    // 42억
+    printf("s = %u\n", s3);
+
+    long s4 = 40000000000;
+    // 400억
+    printf("s = %ld\n", s4);
   
   return 0;
 }
